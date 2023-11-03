99e8d3f212d40b0aaa46b6eeae45afdf95b06fd6
chara <blocklu@163.com>
2023-11-03 23:16:33 +0800

    Build vendor.qti.hardware.capabilityconfigstore@1.0.vendor

--- sdm845.mk
+++ sdm845.mk
@@ -103,7 +103,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.1-impl:32 \
     audio.bluetooth.default \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.capabilityconfigstore@1.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
