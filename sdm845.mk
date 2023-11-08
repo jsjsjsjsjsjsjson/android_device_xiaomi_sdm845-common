c8f3c69f918c7419a744f9b603204c6b5f67334e
chara <blocklu@163.com>
2023-11-08 11:51:23 +0800

    Reset Power HAL

--- sdm845.mk
+++ sdm845.mk
@@ -286,8 +286,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.2.vendor
+    android.hardware.power@1.2.vendor \
+    android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
